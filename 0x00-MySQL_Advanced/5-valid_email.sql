f513510aed03c79282e22a7bd55fa584007b25b9
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-19 09:48:44 +0000

    Corected the email validation systax

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -4,9 +4,11 @@
 DELIMITER $$ ;
 CREATE TRIGGER user_trigger BEFORE UPDATE ON users FOR EACH ROW
 BEGIN
-IF OLD.email <> NEW.email AND OLD.valid_email = 0 THEN
-SET NEW.valid_email = 1;
-ELSEIF OLD.email <> NEW.email AND OLD.valid_email = 1 THEN
+-- IF OLD.email <> NEW.email AND OLD.valid_email = 0 THEN
+-- SET NEW.valid_email = 1;
+-- ELSEIF OLD.email <> NEW.email AND OLD.valid_email = 1 THEN
+-- SET NEW.valid_email = 0;
+IF OLD.email <> NEW.email THEN
 SET NEW.valid_email = 0;
 END IF;
 END $$
