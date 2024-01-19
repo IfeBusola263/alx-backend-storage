1d9b074c76132833c9294d4e1da0bd2d957a3cec
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-19 09:39:07 +0000

    Re-positioned the index arguments

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,4 +1,4 @@
 -- Create index for name in the names table
 -- Index idx_name_first indexes name and the first letter of name
 
-CREATE INDEX idx_name_first ON names (name, (name(1));
+CREATE INDEX idx_name_first ON names (name(1));
