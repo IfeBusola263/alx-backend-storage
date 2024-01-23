c0e3d01b7ab6d34246477df5655fd8904fcdedae
Busola <ifebusolaj95@gmail.com>
2024-01-23 14:54:05 -0800

    Implemented an update function for the collection

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -12,5 +12,5 @@ def update_topics(mongo_collection, name, topics):
     '''
 
 
-    result = mongo_collection.update_one({'name': name},
+    result = mongo_collection.update_many({'name': name},
                                           {'$set': {'topics': topics}})
