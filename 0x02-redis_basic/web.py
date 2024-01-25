65f00dad617a4cf45a9f2b7cbab9eee3ddb58feb
IfeBusola <ifebusolaj95@gmail.com>
2024-01-25 03:11:12 +0000

    Not well tested

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python3
+'''
+This module hosues a function that makes
+a get request to a url
+'''
+
 import requests
 import redis
 import time
 from functools import wraps
 
+
 def cache_page(expiration_time=10):
+    '''
+    Decorator to inspected the url.
+    and record the counts
+    '''
     def decorator(func):
+        '''
+        A decorator to wrap the function.
+        '''
         @wraps(func)
         def wrapper(*args, **kwargs):
+            '''
+            The function wrapper
+            Carrying out the count.
+            '''
             url = args[0]  # Assuming the URL is the first argument
-
+            redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
             # Key for tracking access count
             count_key = f"count:{url}"
 
@@ -21,6 +38,7 @@ def cache_page(expiration_time=10):
             if cached_result:
                 # Increment access count
                 redis_client.incr(count_key)
+                redis_client.save()
                 return cached_result.decode('utf-8')
 
             # If not cached, fetch the page
@@ -31,16 +49,19 @@ def cache_page(expiration_time=10):
 
             # Increment access count
             redis_client.incr(count_key)
+            redis_client.save()
 
             return result
 
         return wrapper
     return decorator
 
-# Connect to Redis
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 @cache_page()
 def get_page(url: str) -> str:
+    '''
+    This function makes a get request and check number of times
+    the url is called
+    '''
     response = requests.get(url)
     return response.text
